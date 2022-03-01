b9990aa3294674b11c132ff31037bb5ec88c96e6
Shreyas Dhanakshirur <dshreyas888@gmail.com>
2022-03-01 19:31:23 +0530

    make it better

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var corsOptions = {
   app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://manage-task-app.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin',  'https://manage-task-app.herokuapp.com/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
